81cc3673b05512506b92a0d94db93890bb37e1f7
E_Davron <ddavronjon123@gmail.com>
2023-07-24 13:58:13 +0500

    and

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('title', 'dashboard - Laravel Admin Panel With Login And Registration')
+
+@section('contents')
+<div class="row">
+    Dashboard
+</div>
+@endsection
